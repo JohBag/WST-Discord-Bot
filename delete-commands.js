f87d186a99a1b5db57e2686d81d88cd869fcb120
johbag <johan.bagerfeldt@gmail.com>
2022-12-09 13:45:36 +0100

    Added option to update only locally

--- delete-commands.js
+++ delete-commands.js
@@ -4,12 +4,16 @@ import { load } from './json_manager.js';
 const config = load('config');
 const rest = new REST({ version: '10' }).setToken(config.token);
 
+const mode = process.argv[2] ?? 'global'
+
 // for guild-based commands
 rest.put(Routes.applicationGuildCommands(config.clientId, config.guildId), { body: [] })
     .then(() => console.log('Successfully deleted all guild commands.'))
     .catch(console.error);
 
-// for global commands
-rest.put(Routes.applicationCommands(config.clientId), { body: [] })
-    .then(() => console.log('Successfully deleted all application commands.'))
-    .catch(console.error);
+if (mode == 'global') {
+    // for global commands
+    rest.put(Routes.applicationCommands(config.clientId), { body: [] })
+        .then(() => console.log('Successfully deleted all application commands.'))
+        .catch(console.error);
+}
--- deploy-commands.js
+++ deploy-commands.js
@@ -3,40 +3,36 @@ import { load } from './json_manager.js';
 import * as commands from './index/commands.js'
 
 const config = load('config');
+const mode = process.argv[2] ?? 'global'
 
 const commandsData = [];
-// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const command of Object.values(commands)) {
 	commandsData.push(command.data.toJSON());
 }
 
-// Construct and prepare an instance of the REST module
 const rest = new REST({ version: '10' }).setToken(config.token);
-
-// and deploy your commands!
 
 (async () => {
 	try {
 		console.log(`Started refreshing ${commandsData.length} application (/) commands.`);
 
-		// The put method is used to fully refresh all commands in the guild with the current set
-		// Specific server
-		/*
-		const data = await rest.put(
-			Routes.applicationGuildCommands(config.clientId, config.guildId),
-			{ body: commandsData },
-		);
-		*/
-
-		// Global
-		const data = await rest.put(
-			Routes.applicationCommands(config.clientId),
-			{ body: commandsData },
-		);
+		let data;
+		if (mode == 'global') {
+			console.log("Deploying commands globally");
+			data = await rest.put(
+				Routes.applicationCommands(config.clientId),
+				{ body: commandsData },
+			);
+		} else { // Test server
+			console.log("Deploying commands locally");
+			data = await rest.put(
+				Routes.applicationGuildCommands(config.clientId, config.guildId),
+				{ body: commandsData },
+			);
+		}
 
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
-		// And of course, make sure you catch and log any errors!
 		console.error(error);
 	}
 })(); 
