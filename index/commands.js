73fd5edffaeee023150a29162f8f6ad1ecf40580
johbag <johan.bagerfeldt@gmail.com>
2022-12-09 13:45:45 +0100

    New command

--- index/commands.js
+++ index/commands.js
@@ -4,5 +4,6 @@ import log from '../commands/log.js'
 import roll from '../commands/roll.js'
 import vote from '../commands/vote.js'
 import endvote from '../commands/endvote.js'
+import paint from '../commands/paint.js'
 
-export { ask, coin, log, roll, vote, endvote };
+export { ask, coin, log, roll, vote, endvote, paint };
