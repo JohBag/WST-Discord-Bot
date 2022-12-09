10a956575b3b41a8ea6c8c02ddcc1c14c014e34d
johbag <johan.bagerfeldt@gmail.com>
2022-12-09 17:33:46 +0100

    change to mode behavior

--- delete-commands.js
+++ delete-commands.js
@@ -6,14 +6,14 @@ const rest = new REST({ version: '10' }).setToken(config.token);
 
 const mode = process.argv[2] ?? 'global'
 
-// for guild-based commands
-rest.put(Routes.applicationGuildCommands(config.clientId, config.guildId), { body: [] })
-    .then(() => console.log('Successfully deleted all guild commands.'))
-    .catch(console.error);
-
 if (mode == 'global') {
     // for global commands
     rest.put(Routes.applicationCommands(config.clientId), { body: [] })
         .then(() => console.log('Successfully deleted all application commands.'))
+        .catch(console.error);
+} else {
+    // for guild-based commands
+    rest.put(Routes.applicationGuildCommands(config.clientId, config.guildId), { body: [] })
+        .then(() => console.log('Successfully deleted all guild commands.'))
         .catch(console.error);
 }
