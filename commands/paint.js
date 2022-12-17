a4c5348fee46313338205a41cf883aebc3ecc59d
johbag <johan.bagerfeldt@gmail.com>
2022-12-17 10:21:25 +0100

    Send image instead of URL for easier use

--- commands/paint.js
+++ commands/paint.js
@@ -1,8 +1,13 @@
 import { Configuration, OpenAIApi } from "openai";
 import { EmbedBuilder, SlashCommandBuilder } from 'discord.js';
 import { load } from '../json_manager.js';
+import fs from 'fs';
 
 const config = load('config');
+const configuration = new Configuration({
+    apiKey: config.apiKey,
+});
+const openai = new OpenAIApi(configuration);
 
 export default {
     data: new SlashCommandBuilder()
@@ -17,23 +22,36 @@ export default {
 
         const prompt = interaction.options.getString('prompt');
 
-        const configuration = new Configuration({
-            apiKey: config.apiKey,
-        });
-        const openai = new OpenAIApi(configuration);
-        const response = await openai.createImage({
-            prompt: prompt,
-            n: 1,
-            size: "256x256",
-        });
-        const imageUrl = response.data.data[0].url;
+        await generateImage(prompt);
 
         const embed = new EmbedBuilder()
             .setTitle(prompt)
-            .setImage(imageUrl);
+            .setImage('attachment://image.png');
 
         return interaction.editReply({
-            embeds: [embed]
+            embeds: [embed],
+            files: ['./image.png']
         });
     },
 };
+
+async function generateImage(prompt) {
+    const response = await openai.createImage({
+        prompt: prompt,
+        n: 1,
+        size: "256x256",
+        response_format: 'b64_json'
+    });
+    const b64 = response.data.data[0].b64_json;
+
+    await convertBase64ToImage(b64);
+}
+
+async function convertBase64ToImage(data) {
+    // Convert base64 to buffer
+    const buffer = Buffer.from(data, "base64");
+    // Create image from buffer
+    fs.writeFileSync('image.png', buffer);
+}
+
+generateImage('A fox with a crown');
