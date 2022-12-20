d3949b64e5b707cf13ebfb5780f3afe74c0e6de6
johbag <johan.bagerfeldt@gmail.com>
2022-12-20 21:59:43 +0100

    New intents

--- bot.js
+++ bot.js
@@ -3,7 +3,13 @@ import { load } from './json_manager.js';
 import * as events from './index/events.js'
 import * as commands from './index/commands.js'
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent
+	]
+});
 const config = load('config');
 
 // Load events
@@ -13,12 +19,14 @@ for (const event of Object.values(events)) {
 	} else {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
+	console.log(`Loaded event: ${event.name}`);
 }
 
 // Load commands
 client.commands = new Collection();
 for (const command of Object.values(commands)) {
 	client.commands.set(command.data.name, command);
+	console.log(`Loaded command: ${command.data.name}`);
 }
 
 client.login(config.token);
