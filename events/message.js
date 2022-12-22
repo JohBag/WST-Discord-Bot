4b5283077652cd09ea12673553f2eb034f4d391f
johbag <johan.bagerfeldt@gmail.com>
2022-12-22 11:30:59 +0100

    Respond to messages

--- events/message.js
+++ events/message.js
@@ -1,45 +1,86 @@
 import getAIResponse from "../common/gpt-3.js";
+import convertToSpeech from '../common/syntheticSpeech.js';
+import emojiRegex from "emoji-regex";
+import { load } from '../json_manager.js';
+
+const config = load('config');
+
+const username = 'Botty McBotface'
+const nicknames = [
+    'botty',
+    'Botty',
+]
 
 export default {
     name: 'messageCreate',
     async execute(interaction) {
-        console.log('Message!');
-
-        let channelID = interaction.channelId;
-        messages[channelID] = messages[channelID] || [];
-
-        let channel = messages[channelID];
-
-        let message = {
-            author: interaction.author.username,
-            content: interaction.content
-        }
-
-        channel.push(message);
-
-        if (channel.length > 20) {
-            channel.shift();
-        }
-
         if (interaction.author.bot) {
             return;
         }
 
-        interpretMessage(channelID);
+        // Random chance to respond
+        if (!shouldRespond(interaction, config.clientId)) {
+            return;
+        }
+
+        console.log("Reacting to:\n" + interaction.content);
+
+        // Get channel conversation
+        let conversation = "";
+        await interaction.channel.messages.fetch({ limit: 10 }).then(messages => {
+            const name = interaction.member.nickname ?? interaction.user.username;
+            messages.reverse().forEach(message => conversation += name + ": " + message.content + "\n");
+        });
+
+        // React with emoji
+        let reaction = await getAIResponse("Respond with the unicode of the discord emote you would use to react to the last message in the following conversation. \n" + conversation + " END_OF_PROMPT");
+        reaction = reaction.substring(reaction.indexOf(':'));
+        console.log(reaction);
+
+        // Check if emoji is valid
+        const regex = emojiRegex();
+        for (const match of reaction.matchAll(regex)) {
+            const emoji = match[0];
+            interaction.react(emoji);
+            break;
+        }
+
+        // Generate response
+        let response = await getAIResponse("You are the user '" + username + "'. Respond with the reply you would send to the last message in the following conversation.\n" + conversation + " END_OF_PROMPT");
+        response = response.replace("Warseeker Test Bot: ", '');
+        console.log(response);
+
+        // Convert to synthetic speech
+        const file = await convertToSpeech(response);
+
+        // Prepare message
+        let msg = { content: response };
+        if (file != null) {
+            msg['files'] = [{
+                attachment: file,
+                name: file
+            }];
+        }
+
+        // Send
+        interaction.channel.send(msg);
     },
 };
 
-let messages = {};
-
-async function interpretMessage(channelID) {
-    let conversation = "";
-
-    const channel = messages[channelID];
-    for (const i of channel) {
-        conversation += i.author + ": " + i.content + "\n";
+function shouldRespond(interaction, clientId) {
+    if (!config.reactChannels.includes(interaction.channelId)) {
+        return false;
     }
-    console.log(conversation);
 
-    const response = await getAIResponse("What discord emote would you use to react to the last message in the following conversation?\n" + conversation);
-    console.log(response);
+    const rng = Math.random();
+    if (rng > 0.9) {
+        console.log("Random reply (" + rng + ")");
+        return true;
+    }
+    if (nicknames.some(name => interaction.content.includes(name)) || interaction.mentions.users.has(clientId)) {
+        console.log("Responding to mention");
+        return true;
+    }
+
+    return false;
 }
