199f536c1ad80cd48cddb6f07a478633df0a8408
johbag <johan.bagerfeldt@gmail.com>
2022-12-22 11:29:54 +0100

    Removed local option

--- delete-commands.js
+++ delete-commands.js
@@ -4,16 +4,7 @@ import { load } from './json_manager.js';
 const config = load('config');
 const rest = new REST({ version: '10' }).setToken(config.token);
 
-const mode = process.argv[2] ?? 'global'
-
-if (mode == 'global') {
-    // for global commands
-    rest.put(Routes.applicationCommands(config.clientId), { body: [] })
-        .then(() => console.log('Successfully deleted all application commands.'))
-        .catch(console.error);
-} else {
-    // for guild-based commands
-    rest.put(Routes.applicationGuildCommands(config.clientId, config.guildId), { body: [] })
-        .then(() => console.log('Successfully deleted all guild commands.'))
-        .catch(console.error);
-}
+// for global commands
+rest.put(Routes.applicationCommands(config.clientId), { body: [] })
+    .then(() => console.log('Successfully deleted all application commands.'))
+    .catch(console.error);
--- deploy-commands.js
+++ deploy-commands.js
@@ -3,7 +3,6 @@ import { load } from './json_manager.js';
 import * as commands from './index/commands.js'
 
 const config = load('config');
-const mode = process.argv[2] ?? 'global'
 
 const commandsData = [];
 for (const command of Object.values(commands)) {
@@ -17,19 +16,11 @@ const rest = new REST({ version: '10' }).setToken(config.token);
 		console.log(`Started refreshing ${commandsData.length} application (/) commands.`);
 
 		let data;
-		if (mode == 'global') {
-			console.log("Deploying commands globally");
-			data = await rest.put(
-				Routes.applicationCommands(config.clientId),
-				{ body: commandsData },
-			);
-		} else { // Test server
-			console.log("Deploying commands locally");
-			data = await rest.put(
-				Routes.applicationGuildCommands(config.clientId, config.guildId),
-				{ body: commandsData },
-			);
-		}
+		console.log("Deploying commands globally");
+		data = await rest.put(
+			Routes.applicationCommands(config.clientId),
+			{ body: commandsData },
+		);
 
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
