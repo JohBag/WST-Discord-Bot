9d348e90120aa17c185ab23aee1e740eecb94685
johbag <johan.bagerfeldt@gmail.com>
2023-01-28 10:58:29 +0100

    Added comments

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -6,9 +6,11 @@ const config = load('config');
 export default {
     name: 'messageDelete',
     async execute(interaction) {
+        // Check if the message is a vote
         const id = interaction.id;
         let votes = load('votes');
         if (id in votes) {
+            // Delete the vote
             delete votes[id];
             save('votes', votes);
             console.log("Deleted vote: " + id);
