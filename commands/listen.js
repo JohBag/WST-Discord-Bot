d6451c26ee62b68b5ab84ffb015da4dc71884b16
johbag <johan.bagerfeldt@gmail.com>
2023-02-18 22:58:39 +0100

    Update listen.js

--- commands/listen.js
+++ commands/listen.js
@@ -133,7 +133,7 @@ async function respond(username) {
     await textToSpeech(response);
 
     // Play response
-    const loc = process.cwd() + '\\';
+    const loc = process.cwd() + '\/';
     console.log("Playing " + loc + 'SyntheticSpeech.mp3')
     let resource = createAudioResource(loc + 'SyntheticSpeech.mp3');
     player.play(resource);
