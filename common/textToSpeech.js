3d350cb2b5c6ec4091c1d40d93930c0b11702216
johbag <johan.bagerfeldt@gmail.com>
2023-03-10 19:35:17 +0100

    changed file name

--- common/textToSpeech.js
+++ common/textToSpeech.js
@@ -9,25 +9,33 @@ export default async function textToSpeech(text) {
     const speechConfig = sdk.SpeechConfig.fromSubscription(secrets.speechKey, secrets.speechRegion);
     const audioConfig = sdk.AudioConfig.fromAudioFileOutput(fileName);
 
-    // Voice
-    speechConfig.speechSynthesisVoiceName = "en-GB-RyanNeural";
     speechConfig.speechSynthesisOutputFormat = sdk.SpeechSynthesisOutputFormat.Audio48Khz192KBitRateMonoMp3;
 
     // Create the speech synthesizer.
     var synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
 
+    const voice = { lang: "en-GB", name: "en-GB-RyanNeural", volume: "+0.00%", rate: "+0.00%", pitch: "+0.00%" };
+    const ssml =
+        `<speak xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xmlns:emo="http://www.w3.org/2009/10/emotionml" version="1.0" xml:lang="${voice.lang}">
+            <voice name="${voice.name}">
+                <prosody rate="${voice.rate}" volume="${voice.volume}" pitch="${voice.pitch}">
+                    ${text}
+                </prosody>
+            </voice>
+        </speak>`;
+
     // Start the synthesizer and wait for a result.
-    let promise = new Promise((resolve) => {
-        synthesizer.speakTextAsync(text,
-            function (result) {
-                if (result.reason != sdk.ResultReason.SynthesizingAudioCompleted) {
-                    console.error("Speech synthesis canceled, " + result.errorDetails +
-                        "\nDid you set the speech resource key and region values?");
+    await new Promise((resolve) => {
+        synthesizer.speakSsmlAsync(ssml,
+            result => {
+                if (result.errorDetails) {
+                    console.error(result.errorDetails);
                     fileName = null;
                 }
                 synthesizer.close();
                 synthesizer = null;
                 resolve();
+                console.log("Speech synthesis succeeded.")
             },
             function (err) {
                 console.trace("err - " + err);
@@ -36,7 +44,6 @@ export default async function textToSpeech(text) {
                 resolve();
             });
     });
-    await promise;
 
     return fileName;
 }
