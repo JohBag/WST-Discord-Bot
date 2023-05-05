aea4230b08af292491f23b228618f7426391e1e7
JohBag <johan.bagerfeldt@gmail.com>
2023-05-05 09:40:01 +0200

    Update commands.js

--- index/commands.js
+++ index/commands.js
@@ -4,5 +4,6 @@ import roll from '../commands/roll.js'
 import vote from '../commands/vote.js'
 import paint from '../commands/paint.js'
 import listen from '../commands/listen.js'
+import ask from '../commands/ask.js'
 
-export { coin, log, roll, vote, paint, listen };
+export { coin, log, roll, vote, paint, listen, ask };
