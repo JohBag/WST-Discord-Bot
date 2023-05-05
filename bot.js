0ae1c711325a547c7f3bc97b905e64437ec31917
JohBag <johan.bagerfeldt@gmail.com>
2023-05-05 09:38:46 +0200

    added safeHandler

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 import { Client, Collection, GatewayIntentBits } from 'discord.js';
 import { load } from './json_manager.js';
-import log from './common/logger.js';
 import * as events from './index/events.js'
 import * as commands from './index/commands.js'
 
@@ -14,20 +13,33 @@ const client = new Client({
 });
 const secrets = load('secrets');
 
-// Load events
-for (const event of Object.values(events)) {
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-	log(`Loaded event: ${event.name}`);
+function safeHandler(handler) {
+	return async (...args) => {
+		try {
+			await handler(...args);
+		} catch (error) {
+			console.error(`Error in event handler: ${error}`);
+		}
+	};
 }
+
+for (const event of Object.values(events)) {
+	const handler = safeHandler(event.execute);
+
+	if (event.once) {
+		client.once(event.name, handler);
+	} else {
+		client.on(event.name, handler);
+	}
+
+	console.log(`Loaded event: ${event.name}`);
+}
+
 // Load commands
 client.commands = new Collection();
 for (const command of Object.values(commands)) {
 	client.commands.set(command.data.name, command);
-	log(`Loaded command: ${command.data.name}`);
+	console.log(`Loaded command: ${command.data.name}`);
 }
 
 client.login(secrets.token);
