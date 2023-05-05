ec430392bbd267e4c03a0f58be7f12ad1b662b7d
JohBag <johan.bagerfeldt@gmail.com>
2023-05-05 09:40:45 +0200

    Improved modularity and prompt handling

--- events/message.js
+++ events/message.js
@@ -1,8 +1,9 @@
-import getAIResponse from "../common/gpt-3.js";
+import getAIResponse from "../common/gpt.js";
 import textToSpeech from '../common/textToSpeech.js';
-import emojiRegex from "emoji-regex";
 import { load } from '../json_manager.js';
 import log from '../common/logger.js';
+import generateImage from '../common/image.js';
+import { EmbedBuilder } from 'discord.js';
 
 const secrets = load('secrets');
 const config = load('config');
@@ -18,128 +19,131 @@ export default {
             return;
         }
 
-        if (config.reactChannels.includes(interaction.channelId)) {
-            // Check if user is responding to other users
-            const mentioned = interaction.mentions.users
-            if (mentioned.size > 0 && !(mentioned.has(secrets.clientId))) {
-                return;
-            }
-        } else {
-            // Random chance to respond
-            if (!shouldRespond(interaction, secrets.clientId)) {
-                return;
-            }
-        }
+        const channelID = interaction.channel.id;
+        const settings = getSettings(channelID);
+        console.log(settings);
 
-        // Get channel conversation
-        const messages = await interaction.channel.messages.fetch({ limit: 20 });
-        const arr = Array.from(messages.values());
-        const cutoffIndex = arr.findIndex(message => message.content === cutoff);
-        const messagesUntilCutoff = cutoffIndex !== -1 ? arr.slice(0, cutoffIndex) : arr;
-        let conversation = await Promise.all(messagesUntilCutoff.map(async (message) => {
-            const member = await interaction.guild.members.fetch(message.author.id);
-            const username = member.nickname || message.author.username;
-            const role = username == name ? "assistant" : "user";
-
-            return { role: role, content: `${username}: ${message.content}` };
-        }));
-        if (conversation.length === 0) {
+        if (!shouldRespond(interaction, settings)) {
             return;
         }
-        conversation = conversation.reverse();
 
-        /*
-        // React with emoji
-        let reaction = await getAIResponse(`As ${name}, provide the unicode of a discord emoji suitable for the last message.`, conversation);
-        reaction = reaction.substring(reaction.indexOf(':'));
- 
-        // React if emoji is valid
-        const regex = emojiRegex();
-        for (const match of reaction.matchAll(regex)) {
-            const emoji = match[0];
-            interaction.react(emoji);
-            break;
+        const context = await getContext(interaction, settings.messageLimit);
+        console.log(context);
+        if (context.length === 0) {
+            return;
         }
-        */
-
-        const prompt = `
-        You're a fun and talkative person named ${name}, commonly referred to as Botty. 
-        You're in the World of Warcraft guild 'Warseeker Tribe' Discord server, a place for friends to hang out and chat.
-        Write in a casual and emotive style, with occasional emojis.
-        When giving information, do so in a simple or humorous way.
-        Current date: ${new Date()}.`;
 
         // Generate response
         let response = await getAIResponse(
-            prompt,
-            conversation
+            config.basePrompt + settings.prompt,
+            context
         );
         if (!response) {
             return;
         }
 
         // Split response into pieces of 2000 characters or less (discord limit)
-        const chunks = [];
-        if (response.length > 2000) {
-            while (response.length > 2000) {
-                const lastNewLine = response.lastIndexOf("\n", 2000);
-                const lastCodeBlock = response.lastIndexOf("```", 2000);
-                const index = Math.min(lastNewLine === -1 ? 2000 : lastNewLine, lastCodeBlock === -1 ? 2000 : lastCodeBlock);
-                if (index === -1) {
-                    break;
+        const messages = splitResponse(response);
+
+        for (const message of messages) {
+            let msg = { content: message, files: [] };
+
+            if (settings.textToSpeech) {
+                const speechFile = await textToSpeech(message);
+                if (speechFile != null) {
+                    msg.files.push({
+                        attachment: speechFile,
+                        name: speechFile
+                    });
                 }
-                chunks.push(response.substring(0, index));
-                response = response.substring(index + 1);
-            }
-        }
-        else {
-            chunks.push(response);
-        }
-
-        // Send chunks
-        for (const chunk of chunks) {
-            // Convert to synthetic speech
-            const file = await textToSpeech(chunk);
-
-            // Prepare message
-            let msg = { content: chunk };
-            if (file != null) {
-                msg['files'] = [{
-                    attachment: file,
-                    name: file
-                }];
             }
 
-            // Send
             interaction.channel.send(msg);
         }
     },
 };
 
-function shouldRespond(interaction, clientId) {
-    // Check if bot is allowed in channel
-    const whitelist = config.reactWhitelist;
-    if (whitelist.length > 0 && !whitelist.includes(interaction.channelId)) {
+function getSettings(channelID) {
+    const { default: defaultSettings, [channelID]: channelSettings = {} } = config.channels;
+    return { ...defaultSettings, ...channelSettings };
+}
+
+async function getContext(interaction, messageLimit) {
+    // Get channel conversation
+    const messages = await interaction.channel.messages.fetch({ limit: messageLimit });
+    const arr = Array.from(messages.values());
+
+    const cutoffIndex = arr.findIndex(message => message.content === cutoff);
+    const messagesUntilCutoff = cutoffIndex !== -1 ? arr.slice(0, cutoffIndex) : arr;
+
+    let conversation = await Promise.all(messagesUntilCutoff.map(async (message) => {
+        const member = await interaction.guild.members.fetch(message.author.id);
+        const username = member.nickname || message.author.username;
+        const role = username == name ? "assistant" : "user";
+
+        return { role: role, content: `${username}: ${message.content}` };
+    }));
+
+    return conversation.reverse();
+}
+
+function isInBlacklistedChannel(channelId) {
+    return config.blacklist.includes(channelId);
+}
+
+function hasBotMention(mentions) {
+    return mentions.users.size > 0 && mentions.users.has(secrets.clientId);
+}
+
+function hasBotNickname(content) {
+    return nicknames.some(name => content.toLowerCase().includes(name));
+}
+
+function shouldReactRandomly(reactChance) {
+    const rng = Math.random();
+    return rng > (1 - reactChance);
+}
+
+function shouldRespond(interaction, settings) {
+    const { channelId, mentions, content } = interaction;
+
+    if (isInBlacklistedChannel(channelId)) {
         return false;
-    } else {
-        const blacklist = config.reactBlacklist;
-        if (blacklist.includes(interaction.channelId)) {
-            return false;
-        }
     }
 
-    // Check if bot is mentioned
-    if (nicknames.some(name => interaction.content.toLowerCase().includes(name)) || interaction.mentions.users.has(clientId)) {
+    if (!hasBotMention(mentions) && hasBotNickname(content)) {
         log("Responding to mention");
         return true;
     }
 
-    // Random chance to appear
-    const rng = Math.random();
-    if (rng > (1 - config.reactChance)) {
-        log("Random reply (" + rng + ")");
+    if (shouldReactRandomly(settings.reactChance)) {
+        log(`Random reply (${Math.random()})`);
         return true;
     }
 
     return false;
+}
+
+function splitResponse(response) {
+    const chunks = [];
+
+    while (response.length) {
+        const splitIndex = response.length <= 2000 ? response.length : findSplitIndex(response);
+        chunks.push(response.substring(0, splitIndex));
+        response = response.substring(splitIndex).trim();
+    }
+
+    return chunks;
+}
+
+function findSplitIndex(response) {
+    const lastNewLine = response.lastIndexOf("\n", 2000);
+    const lastCodeBlock = response.lastIndexOf("```", 2000);
+
+    const splitIndex = Math.min(
+        lastNewLine > -1 ? lastNewLine : 2000,
+        lastCodeBlock > -1 ? lastCodeBlock : 2000
+    );
+
+    return splitIndex;
 }
