7163068845502f0edc63908ed0eab71d90c0ff18
JohBag <johan.bagerfeldt@gmail.com>
2023-12-09 21:52:30 +0100

    Improved username fetching

--- modules/getUserName.js
+++ modules/getUserName.js
@@ -1,48 +1,14 @@
-export default async function getUsername(message) {
+export default async function getUsername(interaction) {
+    let name = "Unknown user";
     try {
-        let user = message.author;
-        if (message.type == 3) { // Button interaction
-            user = message.user;
-        }
+        console.log(interaction);
 
-        try {
-            return interaction.member.nickname
-        } catch (error) {
-            // Ignore
-        }
-
-        try {
-            const member = await message.guild.members.fetch(user.id);
-            const nickname = member.nickname;
-            if (nickname) {
-                return nickname;
-            }
-        } catch (error) {
-            // Ignore
-        }
-
-        try {
-            const globalName = user.globalName;
-            if (globalName) {
-                return globalName;
-            }
-        } catch (error) {
-            // Ignore
-        }
-
-        try {
-            return user.username;
-        } catch (error) {
-            // Ignore
-        }
-
-        try {
-            message.member.displayName
-        } catch (error) {
-            // Ignore
-        }
+        let user = interaction.author || interaction.user;
+        const member = await interaction.guild.members.fetch(user.id);
+        name = member.displayName || user.displayName || name;
     } catch (error) {
         log(`Error: ${error}`);
-        return "Unknown";
+    } finally {
+        return name;
     }
 }
