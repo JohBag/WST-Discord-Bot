a325e32b6f5edb13bf9c6b64d05dd90a7c4009fb
JohBag <johan.bagerfeldt@gmail.com>
2023-12-09 10:16:35 +0100

    removed redundant commands

--- index/commands.js
+++ index/commands.js
@@ -1,9 +1,6 @@
-import coin from '../commands/coin.js'
-import log from '../commands/log.js'
-import roll from '../commands/roll.js'
-import vote from '../commands/vote.js'
-import paint from '../commands/paint.js'
 import listen from '../commands/listen.js'
-import ask from '../commands/ask.js'
+import log from '../commands/log.js'
+import paint from '../commands/paint.js'
+import vote from '../commands/vote.js'
 
-export { coin, log, roll, vote, paint, listen, ask };
+export { listen, log, paint, vote };
